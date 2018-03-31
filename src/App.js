e130782134ed2b388911ff4862b53deeab1b165c
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-31 15:47:55 +0700

    Box.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 
 import { Word } from './components/Word';
+import { Box } from './components/Box';
 import './App.css';
 
 export class App extends Component {
   render() {
-    const arr = [
-      { en: 'One', vn: 'Mot', isMemorized: true },
-      { en: 'Two', vn: 'Hai', isMemorized: false },
-      { en: 'Three', vn: 'Ba', isMemorized: false },
-      { en: 'Four', vn: 'Bon', isMemorized: true }
-    ];
-    return (
-      <div>
-        { arr.map(w => <Word key={w.en} wordInfo={w} />) }
-      </div>
-    );
+    // const arr = [
+      //   { en: 'One', vn: 'Mot', isMemorized: true },
+      //   { en: 'Two', vn: 'Hai', isMemorized: false },
+      //   { en: 'Three', vn: 'Ba', isMemorized: false },
+      //   { en: 'Four', vn: 'Bon', isMemorized: true }
+      // ];
+      // return (
+      //   <div>
+      //     { arr.map(w => <Word key={w.en} wordInfo={w} />) }
+      //   </div>
+    // );
+    return <Box />;
   }
 }
---
+++ src/components/Box.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class Box extends Component {
+    render() {
+        return <p>Value = 1</p>;
+    }
+}
