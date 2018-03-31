9d6b5afb360666972c8495460e9d5541562f23c7
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-31 15:50:52 +0700

    state

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 export class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 100 };
+    }
     render() {
-        return <p>Value = 1</p>;
+        return <p>Value = {this.state.count}</p>;
     }
 }
