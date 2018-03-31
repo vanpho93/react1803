c0a2c57f8ac086f8e73a9bba9f6bd9148e8264d7
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-31 16:26:24 +0700

    ong

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,8 +4,21 @@ export class Box extends Component {
     constructor(props) {
         super(props);
         this.state = { count: 100 };
+        this.increase = this.increase.bind(this);
     }
+
+    increase() {
+        this.setState({ count: this.state.count + 1 });
+    }
+
     render() {
-        return <p>Value = {this.state.count}</p>;
+        return (
+            <div>
+                <p>Value = {this.state.count}</p>
+                <button onClick={this.increase}>
+                    Increase
+                </button>
+            </div>
+        );
     }
 }
